353b6becab7dabcdc37f52c8e611cc2584096c78
dhruvildake312 <dhruvildake312@gmail.com>
2025-03-24 09:18:16 +0000

    Add 19.go

---
+++ src/19.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x int = 10
+    if x > 5 {
+        fmt.Println("x is greater than 5")
+    } else {
+        fmt.Println("x is not greater than 5")
+    }
+}
